c24760dc0e03aff99be7acba66720e1be35af3f9
Abu Shaleh Md.Toha <122076404+TohaShaleh@users.noreply.github.com>
2024-08-17 12:24:34 +0600

    Time: 8 ms (79.66%), Space: 8.6 MB (16.73%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,20 +1,28 @@
+// class Solution {
+// public:
+//     bool isPowerOfThree(int n) {
+
+//         if(n<0) return false;
+//         int i=0;
+//         bool ans=false;
+//         while(pow(3,i)<=n)
+//         {
+//             if(pow(3,i)==n)
+//             {
+//                 ans=true;
+//                 break;
+//             }
+//             i++;
+//         }
+//         return ans;
+        
+//     }
+// };
+
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-
-        if(n<0) return false;
-        int i=0;
-        bool ans=false;
-        while(pow(3,i)<=n)
-        {
-            if(pow(3,i)==n)
-            {
-                ans=true;
-                break;
-            }
-            i++;
-        }
-        return ans;
-        
+        // The largest power of 3 within the range of 32-bit signed integers is 3^19 = 1162261467
+        return n > 0 && 1162261467 % n == 0;
     }
 };
